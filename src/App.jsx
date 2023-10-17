1a34c17f5258a89fe3057ebc362c1aac3ae3a264
Op-Pautu <pautuop@gmail.com>
2023-10-17 13:06:46 +0530

    Add initial files and folders

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./index.css";
+
+function App() {
+  return (
+    <div className="bg-red-500 text-blue-50 text-5xl">
+      <section>Hero</section>
+      <section>Parallax</section>
+      <section>Services</section>
+      <section>Parallax</section>
+      <section>Portfolio1</section>
+      <section>Portfolio2</section>
+      <section>Portfolio3</section>
+      <section>Contact</section>
+    </div>
+  );
+}
+
+export default App;
