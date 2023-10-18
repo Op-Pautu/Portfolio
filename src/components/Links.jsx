4468e4d9eb98697e24a79204f2322f8ea738d05c
Op-Pautu <pautuop@gmail.com>
2023-10-18 12:12:26 +0530

    Animate menu links

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,14 +1,51 @@
+import { motion } from "framer-motion";
+
 const items = ["Home", "Services", "Portfolio", "Contact", "About"];
+
+const variants = {
+  open: {
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+  closed: {
+    transition: {
+      staggerChildren: 0.05,
+      staggerDirection: -1,
+    },
+  },
+};
+
+const itemVariants = {
+  open: {
+    y: 0,
+    opacity: 1,
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+  },
+};
 
 const Links = () => {
   return (
-    <div className="absolute w-[100%] h-[100%] flex flex-col items-center justify-center gap-5">
+    <motion.div
+      className="absolute w-[100%] h-[100%] flex flex-col items-center justify-center gap-5"
+      variants={variants}
+    >
       {items.map((item) => (
-        <a href={`#${item}`} key={item} className="text-4xl">
+        <motion.a
+          href={`#${item}`}
+          key={item}
+          className="text-4xl"
+          variants={itemVariants}
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.95 }}
+        >
           {item}
-        </a>
+        </motion.a>
       ))}
-    </div>
+    </motion.div>
   );
 };
 
