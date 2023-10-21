de4327ed93bd71ec52c24123ae48a59767ee0351
Op-Pautu <pautuop@gmail.com>
2023-10-21 14:14:54 +0530

    Add portfolio scrolling

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,4 +1,5 @@
-import { motion, useScroll, useSpring } from "framer-motion";
+/* eslint-disable react/prop-types */
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 import { useRef } from "react";
 
 const items = [
@@ -29,17 +30,55 @@ const items = [
 ];
 
 const Single = ({ item }) => {
-  return <section>{item.title}</section>;
-};
-
-const Portfolio = () => {
   const ref = useRef();
 
   const { scrollYProgress } = useScroll({
     target: ref,
-    offset: ["end end", "start start"],
+    // offset: ["start start", "end start"],
   });
 
+  const y = useTransform(scrollYProgress, [0, 1], [-200, 200]);
+
+  return (
+    <section>
+      <div
+        className="flex items-center justify-center w-[100%] h-[100%] overflow-hidden"
+        id="container"
+      >
+        <div
+          id="wrapper"
+          className="max-w-[1366px] h-[100%] m-auto flex gap-[50px] items-center justify-center"
+        >
+          <div className="flex-1 h-[50%]" id="imageContainer" ref={ref}>
+            <img
+              src={item.img}
+              alt="image"
+              className="h-[100%] w-[100%] object-cover"
+            />
+          </div>
+          <motion.div
+            id="textContainer"
+            style={{ y }}
+            className="flex-1 flex flex-col gap-[30px]"
+          >
+            <h2 className="text-[72px]">{item.title}</h2>
+            <p className="text-gray-400 text-[20px]">{item.desc}</p>
+            <button className="bg-[#ffa500] border-none rounded-[10px] p-[10px] w-[200px] text-black cursor-pointer">
+              See Demo
+            </button>
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+const Portfolio = () => {
+  const ref = useRef();
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["end end", "start start"],
+  });
   const scaleX = useSpring(scrollYProgress, {
     stiffness: 100,
     damping: 30,
@@ -49,13 +88,13 @@ const Portfolio = () => {
     <div id="portfolio" ref={ref} className="relative">
       <div
         id="progress"
-        className="sticky top-0 left-0 pt-[50px] text-center text-[#ffa500] text-[36px]"
+        className="sticky top-0 left-0 pt-[50px] text-center text-[66px] text-[#ffa500]"
       >
-        <h1>Featured Works</h1>
+        <h1 className="">Featured Works</h1>
         <motion.div
           style={{ scaleX: scaleX }}
           id="progressBar"
-          className="h-[10px] bg-white"
+          className="h-[5px] opacity-50 bg-white mt-2"
         ></motion.div>
       </div>
       {items.map((item) => (
